0d7e0ae4ed7002f35ab086e0b05192d0404e8a29
Nickfln <nke1@live.com>
2023-06-05 10:39:19 -0300

    first commit

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#F8F8F8',
+        },
+        {
+          name: 'dark',
+          value: '#333333',
+        },
+      ],
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
