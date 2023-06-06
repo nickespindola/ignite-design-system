bee76f04a35c28b114e35e2c84456639938e2e23
Nickfln <nke1@live.com>
2023-06-06 15:55:58 -0300

    adding box component and fixing code

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -118,31 +118,8 @@ var {
   }
 });
 
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite300",
-  borderRadius: "$sm",
-  padding: "$2 $4",
-  border: 0,
-  fontWeight: "bold",
-  color: "$white",
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4"
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
-});
+// src/components/Box.tsx
+var Box = styled("div", {});
 export {
-  Button
+  Box
 };
---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,8 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const Box = styled('div', {
+  
+})
+
+export interface BoxProps extends ComponentProps<typeof Box> {}
