eabdf2adba74b957c4c154953d4dfe9cbeaecb7f
Nickfln <nke1@live.com>
2023-06-12 09:36:31 -0300

    added text and heading components

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,16 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
-    children: 'Example Text',
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis ducimus officia atque numquam, voluptatem, alias, eum fugiat ut illo exercitationem dolorum aperiam voluptate. Inventore quasi quae, sequi vero accusantium amet?',
   },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
---
+++ packages/react/src/components/Heading.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+// Coloca-se por padrão 'h2' por conta de um 'h2' poder ser utilizado múltiplas vezes, em quanto um 'h1' apenas uma vez.
+export const Heading = styled('h2', {
+  fontFamily: '$default',
+  lineHeight: '$shorter',
+  margin: 0,
+  color: '$gray100',
+
+  variants: {
+    size: {
+      sm: { fontSize: '$xl' },
+      md: { fontSize: '$2xl' },
+      lg: { fontSize: '$4xl' },
+      '2xl': { fontSize: '$5xl' },
+      '3xl': { fontSize: '$6xl' },
+      '4xl': { fontSize: '$7xl' },
+      '5xl': { fontSize: '$8xl' },
+      '6xl': { fontSize: '$9xl' },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export interface HeadingProps extends ComponentProps<typeof Heading> {
+  as?: ElementType
+}
