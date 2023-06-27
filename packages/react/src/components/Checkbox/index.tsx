acfa868f98def4dfe55c3f6d2289ef9447b6b3b7
Nickfln <nke1@live.com>
2023-06-27 11:06:47 -0300

    adding Multi Step component

--- packages/react/src/components/Checkbox/index.tsx
+++ packages/react/src/components/Checkbox/index.tsx
@@ -1,5 +1,6 @@
 import { Check } from 'phosphor-react'
 import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
 
 export interface CheckboxProps
   extends ComponentProps<typeof CheckboxContainer> {}
